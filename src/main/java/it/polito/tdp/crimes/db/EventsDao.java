8908f975afa273278d6e40fde65311128b9b16ff
albertodebenedictis <albertodebenedictis@10.0.0.5>
2020-06-02 13:24:02 +0200

    Esercizio finito seguendo la videolezione

--- src/main/java/it/polito/tdp/crimes/db/EventsDao.java
+++ src/main/java/it/polito/tdp/crimes/db/EventsDao.java
@@ -5,53 +5,151 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+
+import it.polito.tdp.crimes.model.Adiacenza;
 import it.polito.tdp.crimes.model.Event;
 
-
 public class EventsDao {
-	
-	public List<Event> listAllEvents(){
-		String sql = "SELECT * FROM events" ;
-		try {
-			Connection conn = DBConnect.getConnection() ;
 
-			PreparedStatement st = conn.prepareStatement(sql) ;
-			
-			List<Event> list = new ArrayList<>() ;
-			
-			ResultSet res = st.executeQuery() ;
-			
-			while(res.next()) {
+	/**
+	 * Ritorna la lista dei mesi distinti in cui si sono verificati i diversi
+	 * crimini
+	 * 
+	 * @return
+	 */
+	public List<Integer> getMesi() {
+
+//		Occhio al distinct nella query
+		String sql = "SELECT DISTINCT Month(reported_date) as mese FROM events";
+
+		List<Integer> mesi = new LinkedList<>();
+
+		try {
+
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet rs = st.executeQuery();
+
+//			Mi prendo l'intero che rappresenta il mese
+			while (rs.next()) {
+				mesi.add(rs.getInt("mese"));
+			}
+			conn.close();
+
+//			Prima li ordiniamo
+			Collections.sort(mesi);
+
+			return mesi;
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+			return null;
+		}
+
+	}
+
+	/**
+	 * Ritorna le categorie di crimini
+	 * 
+	 * @return
+	 */
+	public List<String> getCategorie() {
+
+		String sql = "Select distinct offense_category_id as categoria from events";
+
+		List<String> categorie = new LinkedList<>();
+
+		try {
+
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet rs = st.executeQuery();
+
+//			Mi prendo l'intero che rappresenta il mese
+			while (rs.next()) {
+				categorie.add(rs.getString("categoria"));
+			}
+			conn.close();
+
+			return categorie;
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+			return null;
+		}
+
+	}
+
+	public List<Event> listAllEvents() {
+		String sql = "SELECT * FROM events";
+		try {
+			Connection conn = DBConnect.getConnection();
+
+			PreparedStatement st = conn.prepareStatement(sql);
+
+			List<Event> list = new ArrayList<>();
+
+			ResultSet res = st.executeQuery();
+
+			while (res.next()) {
 				try {
-					list.add(new Event(res.getLong("incident_id"),
-							res.getInt("offense_code"),
-							res.getInt("offense_code_extension"), 
-							res.getString("offense_type_id"), 
-							res.getString("offense_category_id"),
-							res.getTimestamp("reported_date").toLocalDateTime(),
-							res.getString("incident_address"),
-							res.getDouble("geo_lon"),
-							res.getDouble("geo_lat"),
-							res.getInt("district_id"),
-							res.getInt("precinct_id"), 
-							res.getString("neighborhood_id"),
-							res.getInt("is_crime"),
-							res.getInt("is_traffic")));
+					list.add(new Event(res.getLong("incident_id"), res.getInt("offense_code"),
+							res.getInt("offense_code_extension"), res.getString("offense_type_id"),
+							res.getString("offense_category_id"), res.getTimestamp("reported_date").toLocalDateTime(),
+							res.getString("incident_address"), res.getDouble("geo_lon"), res.getDouble("geo_lat"),
+							res.getInt("district_id"), res.getInt("precinct_id"), res.getString("neighborhood_id"),
+							res.getInt("is_crime"), res.getInt("is_traffic")));
 				} catch (Throwable t) {
 					t.printStackTrace();
 					System.out.println(res.getInt("id"));
 				}
 			}
-			
+
 			conn.close();
-			return list ;
+			return list;
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return null ;
+			return null;
 		}
+	}
+
+	public List<Adiacenza> getAdiacenze(String categoria, Integer mese) {
+
+		String sql = "select e1.offense_type_id as v1, e2.offense_type_id as v2, count(distinct(e1.neighborhood_id)) as peso from events as e1, events as e2 where e1.offense_category_id = ? and e2.offense_category_id=? and Month(e1.reported_date) = ? and Month(e2.reported_date) = ? and e1.offense_type_id != e2.offense_type_id and e1.neighborhood_id = e2.neighborhood_id group by e1.offense_type_id, e2.offense_type_id ";
+
+		List<Adiacenza> adiacenze = new LinkedList<>();
+
+		try {
+
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+
+//			Set parametri
+			st.setString(1, categoria);
+			st.setString(2, categoria);
+			st.setInt(3, mese);
+			st.setInt(4, mese);
+
+			ResultSet rs = st.executeQuery();
+
+			while (rs.next()) {
+				
+				adiacenze.add(new Adiacenza(rs.getString("v1"),rs.getString("v2"),rs.getDouble("peso")));
+			}
+			conn.close();
+
+			return adiacenze;
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+			return null;
+		}
+
 	}
 
 }
--- src/main/java/it/polito/tdp/crimes/model/Model.java
+++ src/main/java/it/polito/tdp/crimes/model/Model.java
@@ -1,6 +1,152 @@
 package it.polito.tdp.crimes.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.crimes.db.EventsDao;
 
 public class Model {
+
+	private Graph<String, DefaultWeightedEdge> grafo;
+	private EventsDao dao;
+	private List<String> best;
+
+	public Model() {
+		dao = new EventsDao();
+	}
+
+	public void creaGrafo(String categoria, Integer mese) {
+
+		/**
+		 * NB: Non usiamo in questo caso un idMap per i vertici perchè sono
+		 * rappresentati da stringhe, che hanno già un loro hashCode ed equals che non
+		 * ci fa correre il rischio di considerare due elementi uguali come due diversi
+		 * vertici
+		 * 
+		 */
+
+		this.grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+
+//		Mi faccio dare le adiacenze dal dao
+		List<Adiacenza> adiacenze = this.dao.getAdiacenze(categoria, mese);
+		for (Adiacenza a : adiacenze) {
+
+//			Non sono sicuro che siano strettamente necessari gli IF
+
+//			Se il primo vertice non è stato ancora aggiunto, lo aggiungo
+			if (!this.grafo.containsVertex(a.getV1())) {
+				this.grafo.addVertex(a.getV1());
+			}
+//			Faccio lo stesso per il secondo
+			if (!this.grafo.containsVertex(a.getV2())) {
+				this.grafo.addVertex(a.getV2());
+			}
+//			Stesso per l'arco (se il get mi dà null, vuol dire che non ce l'ho)
+			if (this.grafo.getEdge(a.getV1(), a.getV2()) == null) {
+				Graphs.addEdgeWithVertices(this.grafo, a.getV1(), a.getV2(), a.getPeso());
+			}
+		}
+
+		System.out.println(String.format("Grafo creato con %d vertici e %d archi", this.grafo.vertexSet().size(),
+				this.grafo.edgeSet().size()));
+
+	}
+
+	public List<Integer> getMesi() {
+		return dao.getMesi();
+	}
+
+	public List<String> getCategorie() {
+		return dao.getCategorie();
+	}
+
+	public List<Arco> getArchi() {
+
+		double pesoMedio = 0;
+
+		for (DefaultWeightedEdge arco : this.grafo.edgeSet()) {
+			pesoMedio += this.grafo.getEdgeWeight(arco);
+		}
+		pesoMedio = pesoMedio / this.grafo.edgeSet().size();
+
+		List<Arco> archi = new ArrayList<>();
+
+		for (DefaultWeightedEdge arco : this.grafo.edgeSet()) {
+			if (this.grafo.getEdgeWeight(arco) > pesoMedio) {
+				archi.add(new Arco(this.grafo.getEdgeSource(arco), this.grafo.getEdgeTarget(arco),
+						this.grafo.getEdgeWeight(arco)));
+			}
+		}
+
+		Collections.sort(archi);
+
+		return archi;
+	}
 	
+	/**
+	 * Trova il percorso tra sorgente e destinazione che tocca il numero
+	 * max di vertici
+	 * @param sorgente
+	 * @param destinazione
+	 * @return percorso con numero max vertici
+	 */
+	public List<String> trovaPercorso(String sorgente, String destinazione){
+		
+//		Inizializziamo soluzione parziale e finale
+		List<String> parziale = new ArrayList<>();
+		this.best = new ArrayList<>();
+		
+//		Aggiungiamo il nodo di partenza
+		parziale.add(sorgente);
+		
+		trovaRicorsivo(destinazione, parziale, 0);
+		
+		return this.best;
+	
+	}
+
+	private void trovaRicorsivo(String destinazione, List<String> parziale, int livello) {
+		
+//		CASO TERMINALE: l'ultimo vertice inserito in parziale è uguale alla destinazione
+		if(parziale.get(parziale.size()-1).equals(destinazione)) {
+			
+//			Controllo se è migliore di best
+			if(parziale.size()>best.size()) {
+				this.best = new ArrayList<>(parziale);
+//				Ovviamente la creo nuova altrimenti non aggiungo un bel niente
+			}
+			return;
+		}
+		
+//		scorro i vicini dell'ultimo vertice inserito in parziale,
+//		provo ad aggiungerlo e faccio backtracking
+		for(String vicino: Graphs.neighborListOf(this.grafo, parziale.get(parziale.size()-1))) {
+			
+//			cammino aciclico --> controllo che il vertice non sia già in parziale
+			if(!parziale.contains(vicino)) {
+				
+//				provo ad aggiungere
+				parziale.add(vicino);
+				
+//				lancio la ricorsione
+				trovaRicorsivo(destinazione,parziale,livello+1);
+				
+//				faccio backtracking
+				parziale.remove(parziale.size()-1);
+			}
+			
+		}
+		
+		
+		
+	}
+	
+	
+
 }
